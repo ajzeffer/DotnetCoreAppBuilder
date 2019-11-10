2129235441d280b7a5b855cf962ef291b3eb4145
Adam Zeffer <azeffer@DTAdmins-MacBook-Pro.local>
2019-11-10 09:13:19 -0700

    chore: removing log()

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import Args from './args';
 import DotnetSolution from './solution';
 
 let name = new Args().getArgs('name');
-console.log(name);
 if(name != undefined){
     let dotnet = new DotnetSolution(name).create(); 
 }
